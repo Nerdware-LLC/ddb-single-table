c3791a7c9bdd40bb7409fb3cb3491b2500a28c48
trevor-anderson <trevor@nerdware.cloud>
2023-08-27 14:59:52 -0400

    build(tsc): replace swc with tsc esm+cjs builds

---
+++ scripts/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+############################################################
+# This script creates the ESM and CJS builds of the package.
+
+# First, ensure this script was run from repo root
+if [ ! -f "./scripts/build.sh" ]; then
+  echo "Please run this script from the repo root."
+  exit 1
+fi
+
+# Remove ./dist if it exists to ensure a clean build
+[ -d "./dist" ] && rm -rf ./dist
+
+# Make the ESM build
+tsc --project tsconfig.build-esm.json
+echo '{ "type": "module" }' > dist/esm/package.json
+
+# Make the CJS build
+tsc --project tsconfig.build-cjs.json
+echo '{ "type": "commonjs" }' > dist/cjs/package.json
+
+############################################################
