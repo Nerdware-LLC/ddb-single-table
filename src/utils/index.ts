2112deb86d5c628e4d22856ac3a293a364dd1210
trevor-anderson <trevor@nerdware.cloud>
2023-08-27 15:04:15 -0400

    feat: add util isConvertibleToDate

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,6 @@ export * from "./ddbExpressionHelpers";
 export * from "./errors";
 export * from "./hasDefinedProperty";
 export * from "./hasKey";
+export * from "./isConvertibleToDate";
 export * from "./isType";
 export * from "./safeJsonStringify";
---
+++ src/utils/isConvertibleToDate.ts
@@ -0,0 +1,15 @@
+import dayjs, { type ConfigType } from "dayjs";
+
+/**
+ * This type-guard function tests if the provided `value` can be converted
+ * into a valid Date object.
+ *
+ * @param value The value to test.
+ * @returns `true` if the value can be converted into a valid Date object.
+ */
+export const isConvertibleToDate = (
+  value: unknown
+): value is Exclude<ConfigType, null | undefined> => {
+  // The dayjs ctor only ever throws when given a Symbol or BigInt.
+  return !["symbol", "bigint"].includes(typeof value) && dayjs(value as any).isValid();
+};
