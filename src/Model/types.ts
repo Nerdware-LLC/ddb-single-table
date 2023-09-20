48aa92d98abe37fc550ac7f5915127e279210287
trevor-anderson <trevor@nerdware.cloud>
2023-09-20 12:46:15 -0400

    fix: rm extraneous import

--- src/Model/types.ts
+++ src/Model/types.ts
@@ -1,4 +1,4 @@
-import type { Simplify, ConditionalPick } from "type-fest";
+import type { Simplify } from "type-fest";
 import type { BatchRetryExponentialBackoffConfigs } from "../BatchRequests";
 import type {
   GetItemInput,
