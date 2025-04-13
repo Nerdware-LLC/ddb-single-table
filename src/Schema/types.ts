f9ffb33c6ea81e6e5d8aa93b7c7484e5d89dc8b2
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2025-04-13 10:21:48 -0400

    refactor: extract `SecondaryIndexConfig` type

--- src/Schema/types.ts
+++ src/Schema/types.ts
@@ -146,6 +146,20 @@ export interface BaseAttributeConfig {
   readonly required?: boolean;
 }
 
+/** Secondary index configs, defined within the the attribute config of the index's hash-key. */
+export interface SecondaryIndexConfig {
+  /** The index name */
+  readonly name: string;
+  /** Is global index; pass `false` for local indexes (default: `true`). */
+  readonly global?: boolean;
+  /** The attribute which will serve as the index's range key, if any. */
+  readonly rangeKey?: string;
+  /** `true`: project ALL, `false`: project KEYS_ONLY, `string[]`: project listed attributes */
+  readonly project?: boolean | Array<string>; //
+  /** Don't set this when billing mode is PAY_PER_REQUEST */
+  readonly throughput?: { readonly read: number; readonly write: number };
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 // ATTRIBUTE CONFIG TYPES:
 
@@ -160,18 +174,7 @@ export interface KeyAttributeConfig extends BaseAttributeConfig {
   /** Indicates the attribute is the table's range key (default: `false`) */
   readonly isRangeKey?: boolean;
   /** DynamoDB index configs, provided on the index's hash key. */
-  readonly index?: {
-    /** The index name */
-    readonly name: string;
-    /** Is global index; pass `false` for local indexes (default: `true`). */
-    readonly global?: boolean;
-    /** The attribute which will serve as the index's range key, if any. */
-    readonly rangeKey?: string;
-    /** `true`: project ALL, `false`: project KEYS_ONLY, `string[]`: project listed attributes */
-    readonly project?: boolean | Array<string>; //
-    /** Don't set this when billing mode is PAY_PER_REQUEST */
-    readonly throughput?: { readonly read: number; readonly write: number };
-  };
+  readonly index?: SecondaryIndexConfig;
 }
 
 /** Non-key attribute configs. */
