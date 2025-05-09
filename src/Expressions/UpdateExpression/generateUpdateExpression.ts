da479f51af1572d5ed2fe70e0e765e0f339611aa
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2025-05-09 06:18:12 -0400

    fix: update the logic in `generateUpdateExpression` to work as expected with nested partial objects/arrays

--- src/Expressions/UpdateExpression/generateUpdateExpression.ts
+++ src/Expressions/UpdateExpression/generateUpdateExpression.ts
@@ -1,4 +1,4 @@
-import { getExpressionAttrTokens } from "../helpers.js";
+import { buildAttrPathTokens } from "./buildAttrPathTokens.js";
 import type { GenerateUpdateExpressionOpts } from "./types.js";
 import type { UpdateItemInput } from "../../DdbClientWrapper/types/index.js";
 
@@ -9,54 +9,64 @@ import type { UpdateItemInput } from "../../DdbClientWrapper/types/index.js";
  * - `ExpressionAttributeNames`
  * - `ExpressionAttributeValues`
  *
- * Attribute names and values in the `UpdateExpression` are replaced with token placeholders, which
- * here are derived by removing all non-letter characters from the key, and then adding a num-sign
- * prefix to the EA-Names token and a colon prefix to the the EA-Values token. For example, an
- * attribute `"foo-1"` would yield clause `"#foo = :foo"` in the `UpdateExpression`.
- *
  * `UpdateExpression` Clauses:
  * - The `"SET"` clause includes all attributes which are _not_ explicitly `undefined`.
  * - The `"REMOVE"` clause includes all attributes which are explicitly set to `undefined`.
- * - If **{@link GenerateUpdateExpressionOpts|nullHandling}** is `"REMOVE"` (default), then
+ * - If {@link GenerateUpdateExpressionOpts|nullHandling} is `"REMOVE"` (default), then
  *   attributes with `null` values are added to the `"REMOVE"` clause, otherwise they are
  *   added to the `"SET"` clause.
  */
 export const generateUpdateExpression = (
   itemAttributes: { [attrName: string]: unknown },
   { nullHandling }: GenerateUpdateExpressionOpts = {}
-) => {
+): {
+  [Key in
+    | "UpdateExpression"
+    | "ExpressionAttributeNames"
+    | "ExpressionAttributeValues"]: NonNullable<UpdateItemInput[Key]>;
+} => {
   const shouldAddToRemoveClause =
     nullHandling !== "SET"
       ? (value: unknown) => value === undefined || value === null
       : (value: unknown) => value === undefined;
 
-  const updateExpressionClauses = { SET: "", REMOVE: "" };
-  const ExpressionAttributeNames: UpdateItemInput["ExpressionAttributeNames"] = {};
-  const ExpressionAttributeValues: UpdateItemInput["ExpressionAttributeValues"] = {};
+  const updateExpressionClauses: Record<"SET" | "REMOVE", Array<string>> = { SET: [], REMOVE: [] };
+  const ExpressionAttributeNames: Record<string, string> = {};
+  const ExpressionAttributeValues: Record<string, unknown> = {};
 
-  for (const attributeName in itemAttributes) {
-    const attributeValue = itemAttributes[attributeName];
-    // Get the keys for ExpressionAttribute{Names,Values}
-    const { attrNamesToken, attrValuesToken } = getExpressionAttrTokens(attributeName);
-    // Always update ExpressionAttributeNames
-    ExpressionAttributeNames[attrNamesToken] = attributeName;
-    // Derive and append the appropriate UpdateExpression clause
-    if (shouldAddToRemoveClause(attributeValue)) {
-      updateExpressionClauses.REMOVE += `${attrNamesToken}, `;
+  const recurse = (value: unknown, path: Array<string | number>) => {
+    if (typeof value === "object" && value !== null) {
+      if (Array.isArray(value)) {
+        value.forEach((arrayElement, index) => {
+          recurse(arrayElement, [...path, index]);
+        });
+      } else {
+        for (const key in value) {
+          const keyValue = value[key as keyof typeof value];
+          recurse(keyValue, [...path, key]);
+        }
+      }
     } else {
-      updateExpressionClauses.SET += `${attrNamesToken} = ${attrValuesToken}, `;
-      // Only add to EAV if attrValue will be used, otherwise DDB will throw error
-      ExpressionAttributeValues[attrValuesToken] = attributeValue;
+      const { namePath, valueToken } = buildAttrPathTokens(path, ExpressionAttributeNames);
+      // Derive and append the appropriate UpdateExpression clause
+      if (shouldAddToRemoveClause(value)) {
+        updateExpressionClauses.REMOVE.push(namePath);
+      } else {
+        updateExpressionClauses.SET.push(`${namePath} = ${valueToken}`);
+        ExpressionAttributeValues[valueToken] = value;
+      }
     }
-  }
+  };
 
-  // Combine the clauses into UpdateExpression (slice to rm last comma+space)
+  recurse(itemAttributes, []);
+
+  // Combine the clauses into UpdateExpression
   const UpdateExpression = [
     ...(updateExpressionClauses.SET.length > 0
-      ? [`SET ${updateExpressionClauses.SET.slice(0, -2)}`]
+      ? [`SET ${updateExpressionClauses.SET.join(", ")}`]
       : []),
     ...(updateExpressionClauses.REMOVE.length > 0
-      ? [`REMOVE ${updateExpressionClauses.REMOVE.slice(0, -2)}`]
+      ? [`REMOVE ${updateExpressionClauses.REMOVE.join(", ")}`]
       : []),
   ].join(" ");
 
