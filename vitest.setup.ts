44dc67e80fd461148db8731a34ce5e85dd113e3c
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2025-05-21 06:30:02 -0400

    test(vi): replace AWS-SDK mocks with `aws-sdk-client-mock-vitest` setup

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,37 +1,5 @@
-import { mockClient } from "aws-sdk-client-mock";
+import { allCustomMatcher } from "aws-sdk-client-mock-vitest";
+import { expect } from "vitest";
 
-/**
- * This file is used to set up Vitest for testing. It includes the following:
- * - Mocking "@aws-sdk/client-dynamodb" using `aws-sdk-client-mock`
- * - Mocking "@aws-sdk/lib-dynamodb" using `aws-sdk-client-mock`
- */
-
-vi.mock("@aws-sdk/client-dynamodb", async (importOriginal) => {
-  const {
-    DynamoDBClient: ActualDynamoDBClient, // The mocked client class
-    ...actualExports
-  } = await importOriginal<typeof import("@aws-sdk/client-dynamodb")>();
-
-  const DynamoDBClient = vi.fn(() => mockClient(ActualDynamoDBClient));
-
-  return {
-    DynamoDBClient,
-    ...actualExports,
-  };
-});
-
-vi.mock("@aws-sdk/lib-dynamodb", async (importOriginal) => {
-  const {
-    DynamoDBDocumentClient: ActualDynamoDBDocumentClient, // The mocked client class
-    ...actualExports
-  } = await importOriginal<typeof import("@aws-sdk/lib-dynamodb")>();
-
-  const DynamoDBDocumentClient = {
-    from: vi.fn(() => mockClient(ActualDynamoDBDocumentClient)),
-  };
-
-  return {
-    DynamoDBDocumentClient,
-    ...actualExports,
-  };
-});
+// See https://www.npmjs.com/package/aws-sdk-client-mock-vitest
+expect.extend(allCustomMatcher);
