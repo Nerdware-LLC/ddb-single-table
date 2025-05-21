4793961308fca44540c7e54eaa85ee089d0c79aa
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2025-05-21 10:04:52 -0400

    refactor: add `SCHEMA_TYPE` dict for better usability

--- src/Schema/types/SchemaMetadata.ts
+++ src/Schema/types/SchemaMetadata.ts
@@ -1,29 +1,44 @@
-import type { SetOptional, Simplify } from "type-fest";
+import type { ValueOf, OverrideProperties } from "type-fest";
 
 /**
- * Base type for Schema metadata properties.
+ * Dictionary of supported `Schema` types.
+ */
+export const SCHEMA_TYPE = {
+  MODEL_SCHEMA: "ModelSchema",
+  TABLE_KEYS_SCHEMA: "TableKeysSchema",
+} as const satisfies Record<string, string>;
+
+/**
+ * Union of supported `schemaType` values.
+ */
+export type SchemaType = ValueOf<typeof SCHEMA_TYPE>;
+
+/**
+ * Base type for `Schema` metadata properties.
  */
 export type SchemaMetadata = {
-  schemaType: "ModelSchema" | "TableKeysSchema";
+  schemaType: SchemaType;
   name?: string;
   version?: string;
 };
 
 /**
- * ModelSchema {@link SchemaMetadata|metadata properties}.
+ * `ModelSchema` {@link SchemaMetadata|metadata properties}.
  */
-export type ModelSchemaMetadata = Simplify<
-  SetOptional<SchemaMetadata, "schemaType"> & {
-    schemaType?: "ModelSchema";
+export type ModelSchemaMetadata = OverrideProperties<
+  SchemaMetadata,
+  {
     name: string;
+    schemaType?: "ModelSchema";
   }
 >;
 
 /**
- * TableKeysSchema {@link SchemaMetadata|metadata properties}.
+ * `TableKeysSchema` {@link SchemaMetadata|metadata properties}.
  */
-export type TableKeysSchemaMetadata = Simplify<
-  SetOptional<SchemaMetadata, "schemaType"> & {
+export type TableKeysSchemaMetadata = OverrideProperties<
+  SchemaMetadata,
+  {
     schemaType?: "TableKeysSchema";
   }
 >;
