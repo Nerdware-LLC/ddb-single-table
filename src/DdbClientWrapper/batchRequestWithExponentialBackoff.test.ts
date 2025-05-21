f4388abec0dacece6f0d615223738095db5aac07
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2025-05-21 08:47:15 -0400

    refactor: change how the delay is disabled to speed up all tests

--- src/DdbClientWrapper/batchRequestWithExponentialBackoff.test.ts
+++ src/DdbClientWrapper/batchRequestWithExponentialBackoff.test.ts
@@ -8,47 +8,51 @@ import { batchRequestWithExponentialBackoff } from "./batchRequestWithExponentia
 describe("batchRequestWithExponentialBackoff()", () => {
   const mockSubmitBatchRequest = vi.fn();
 
+  const mockBatchRequestObjects = [{ id: 1 }, { id: 2 }, { id: 3 }];
+
   const getDdbBatchError = (errCode: keyof typeof BatchErrorCode): Error & BatchStatementError => {
     return Object.defineProperty(new Error(errCode), "Code", { value: errCode });
   };
 
   test("successfully processes all batch requests without retries", async () => {
+    // Arrange a simulated successful batch request with no unprocessed items
     mockSubmitBatchRequest.mockResolvedValueOnce([]);
-    const batchRequestObjects = [{ id: 1 }, { id: 2 }];
 
     await expect(
-      batchRequestWithExponentialBackoff(mockSubmitBatchRequest, batchRequestObjects)
+      batchRequestWithExponentialBackoff(mockSubmitBatchRequest, mockBatchRequestObjects)
     ).resolves.not.toThrow();
 
-    expect(mockSubmitBatchRequest).toHaveBeenCalledExactlyOnceWith(batchRequestObjects);
+    expect(mockSubmitBatchRequest).toHaveBeenCalledExactlyOnceWith(mockBatchRequestObjects);
   });
 
   test("retries unprocessed items and eventually succeeds", async () => {
-    const unprocessedItems = [{ id: 2 }];
+    // Arrange unprocessed items returned from the first call
+    const unprocessedItems = mockBatchRequestObjects.slice(-1);
 
     mockSubmitBatchRequest
       .mockResolvedValueOnce(unprocessedItems) // 1st call returns unprocessed items
       .mockResolvedValueOnce([]); //              2nd call returns no unprocessed items
 
-    const batchRequestObjects = [{ id: 1 }, { id: 2 }];
-
     await expect(
-      batchRequestWithExponentialBackoff(mockSubmitBatchRequest, batchRequestObjects)
+      batchRequestWithExponentialBackoff(mockSubmitBatchRequest, mockBatchRequestObjects, {
+        initialDelay: 0, // <-- Zero here eliminates all delays for testing
+      })
     ).resolves.not.toThrow();
 
     expect(mockSubmitBatchRequest).toHaveBeenCalledTimes(2);
-    expect(mockSubmitBatchRequest).toHaveBeenNthCalledWith(1, batchRequestObjects);
+    expect(mockSubmitBatchRequest).toHaveBeenNthCalledWith(1, mockBatchRequestObjects);
     expect(mockSubmitBatchRequest).toHaveBeenNthCalledWith(2, unprocessedItems);
   });
 
   test("throws an error after exceeding maxRetries", async () => {
-    const batchRequestObjects = [{ id: 1 }];
-    mockSubmitBatchRequest.mockResolvedValue([{ id: 1 }]);
+    // Arrange the mock fn to always return unprocessed items until maxRetries is reached
+    mockSubmitBatchRequest.mockResolvedValue(mockBatchRequestObjects);
 
     await expect(
-      batchRequestWithExponentialBackoff(mockSubmitBatchRequest, batchRequestObjects, {
+      batchRequestWithExponentialBackoff(mockSubmitBatchRequest, mockBatchRequestObjects, {
         maxRetries: 3,
         maxDelay: Number.POSITIVE_INFINITY, // Set a long maxDelay to trigger the maxRetries condition
+        initialDelay: 0, // <-- Zero here eliminates the delay between retries
       })
     ).rejects.toThrow(DdbSingleTableError);
 
@@ -56,18 +60,34 @@ describe("batchRequestWithExponentialBackoff()", () => {
   });
 
   test("applies jitter to the delay if useJitter is true", async () => {
-    vi.spyOn(global, "setTimeout");
-    mockSubmitBatchRequest.mockResolvedValueOnce([{ id: 1 }]).mockResolvedValueOnce([]);
+    // Arrange a spy on Math.random to ensure it's invoked when useJitter is true:
+    const mockMathRandomValue = 0.35;
+    const mathRandomSpy = vi.spyOn(Math, "random").mockReturnValue(mockMathRandomValue);
+    // Arrange a spy on setTimeout to check the `delay` value:
+    const setTimeoutSpy = vi
+      .spyOn(global, "setTimeout") // Just call the cb immediately:
+      .mockImplementationOnce((cb) => cb() as any); // eslint-disable-line @typescript-eslint/no-unsafe-return
 
-    const batchRequestObjects = [{ id: 1 }];
+    mockSubmitBatchRequest
+      .mockResolvedValueOnce(mockBatchRequestObjects) // return unprocessed items to trigger delay calculation
+      .mockResolvedValueOnce([]);
 
-    await batchRequestWithExponentialBackoff(mockSubmitBatchRequest, batchRequestObjects, {
+    // Set highly-indicative initialDelay and timeMultiplier values for testing:
+    const initialDelay = 333;
+    const timeMultiplier = 2;
+
+    const expectedDelay = Math.round(mockMathRandomValue * (initialDelay * timeMultiplier ** 1));
+
+    await batchRequestWithExponentialBackoff(mockSubmitBatchRequest, mockBatchRequestObjects, {
       useJitter: true,
-      initialDelay: 10,
+      initialDelay,
+      timeMultiplier,
     });
 
-    expect(setTimeout).toHaveBeenCalled();
-    vi.restoreAllMocks();
+    // Assert that Math.random was called:
+    expect(mathRandomSpy).toHaveBeenCalledOnce();
+    // Assert that setTimeout was called with the expected delay:
+    expect(setTimeoutSpy).toHaveBeenCalledExactlyOnceWith(expect.any(Function), expectedDelay);
   });
 
   test("throws an error if a non-retryable error occurs", async () => {
@@ -84,14 +104,15 @@ describe("batchRequestWithExponentialBackoff()", () => {
   });
 
   test("retries if a retryable error occurs", async () => {
-    const batchError = getDdbBatchError("ProvisionedThroughputExceeded");
-
-    mockSubmitBatchRequest.mockRejectedValueOnce(batchError).mockResolvedValueOnce([]);
-
-    const batchRequestObjects = [{ id: 1 }];
+    // Arrange a retryable error (ProvisionedThroughputExceeded) on the first call
+    mockSubmitBatchRequest
+      .mockRejectedValueOnce(getDdbBatchError(BatchErrorCode.ProvisionedThroughputExceeded))
+      .mockResolvedValueOnce([]);
 
     await expect(
-      batchRequestWithExponentialBackoff(mockSubmitBatchRequest, batchRequestObjects)
+      batchRequestWithExponentialBackoff(mockSubmitBatchRequest, mockBatchRequestObjects, {
+        initialDelay: 0, // <-- Zero here eliminates all delays for testing
+      })
     ).resolves.not.toThrow();
 
     expect(mockSubmitBatchRequest).toHaveBeenCalledTimes(2);
