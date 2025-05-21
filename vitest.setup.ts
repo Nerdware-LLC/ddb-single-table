1caea9f49b7a3aa5fef5c78aa330a172db2e21b6
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2025-05-21 11:21:18 -0400

    refactor: rm unnecessary import

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,5 +1,4 @@
 import { allCustomMatcher } from "aws-sdk-client-mock-vitest";
-import { expect } from "vitest";
 
 // See https://www.npmjs.com/package/aws-sdk-client-mock-vitest
 expect.extend(allCustomMatcher);
