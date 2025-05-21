b4e30a23ce7f96dcedaded766324a934ba05fa98
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2025-05-21 06:24:21 -0400

    refactor: add export of 'NativeAttributeValue'

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,6 +5,7 @@ export * from "./ItemKeys.js";
 export * from "./ItemTypeFromSchema.js";
 export * from "./ItemTypeOpts.js";
 export * from "./ItemUpdateParameters.js";
+export * from "./NativeAttributeValue.js";
 export * from "./NestDepth.js";
 export * from "./SupportedAttributeValueType.js";
 export * from "./TimestampAttributes.js";
