58a17cc3b894a99f70ae34da96d4e00c1b5a899d
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2025-05-26 12:17:18 -0400

    fix: update `itemToReturn` to ensure provided arg is not mutated

--- src/Model/ioActions/transformValues.ts
+++ src/Model/ioActions/transformValues.ts
@@ -1,5 +1,6 @@
-import { hasKey, isFunction } from "@nerdware/ts-type-safety-utils";
+import { isFunction } from "@nerdware/ts-type-safety-utils";
 import type { IOActions, IOAction } from "./types.js";
+import type { BaseItem } from "../../types/index.js";
 
 /**
  * This `IOAction` uses `transformValue` functions (if defined) to transform
@@ -10,6 +11,9 @@ export const transformValues: IOAction = function (
   item,
   { schemaEntries, ioDirection, ...ctx }
 ) {
+  // To avoid mutating the original item, create a new object to return
+  const itemToReturn: BaseItem = { ...item };
+
   // Iterate over schemaEntries
   for (let i = 0; i < schemaEntries.length; i++) {
     const [attrName, attrConfig] = schemaEntries[i];
@@ -17,23 +21,27 @@ export const transformValues: IOAction = function (
     // See if a transformValue fn is defined
     const transformValue = attrConfig.transformValue?.[ioDirection];
     // If schema has transformValue toDB/fromDB, pass the existing value into the fn
-    if (hasKey(item as any, attrName) && isFunction(transformValue)) {
+    if (Object.hasOwn(itemToReturn, attrName) && isFunction(transformValue)) {
       // Get new value; any type mismatches are caught later by the `typeChecking` method
-      const transformedValue = transformValue(item[attrName]);
-      // If transformedValue is not undefined, add it to item
-      if (transformedValue !== undefined) item[attrName] = transformedValue;
+      const transformedValue = transformValue(itemToReturn[attrName]);
+      // If transformedValue is not undefined, add it to itemToReturn
+      if (transformedValue !== undefined) itemToReturn[attrName] = transformedValue;
     }
 
     // Run recursively on nested attributes if parent value exists
-    if (attrConfig.schema && hasKey(item as any, attrName)) {
-      item[attrName] = this.recursivelyApplyIOAction(this.transformValues, item[attrName], {
-        parentItem: item,
-        ioDirection,
-        ...ctx,
-        schema: attrConfig.schema, // <-- overwrites ctx.schema with the nested schema
-      });
+    if (attrConfig.schema && Object.hasOwn(itemToReturn, attrName)) {
+      itemToReturn[attrName] = this.recursivelyApplyIOAction(
+        this.transformValues,
+        itemToReturn[attrName],
+        {
+          parentItem: itemToReturn,
+          ioDirection,
+          ...ctx,
+          schema: attrConfig.schema, // <-- overwrites ctx.schema with the nested schema
+        }
+      );
     }
   }
 
-  return item;
+  return itemToReturn;
 };
