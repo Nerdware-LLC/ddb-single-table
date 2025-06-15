2fc3858275171d9b5dde2ba165a2c843e9b758cb
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2025-06-15 11:05:29 -0400

    feat(exports): add type-only exports for batch ops and model params

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,13 @@ export * from "./Model/index.js";
 export * from "./Schema/index.js";
 export * from "./Table/index.js";
 export * from "./types/index.js";
+
+// TYPE-ONLY EXPORTS:
+
+export type * from "./DdbClientWrapper/types/batchOperationTypes.js";
+
+export type { GenerateUpdateExpressionOpts } from "./Expressions/UpdateExpression/types.js";
+export type { WhereQueryComparisonObject } from "./Expressions/WhereQuery/types.js";
+
+export type * from "./Model/types/clientParamsForModel.js";
+export type * from "./Model/types/ModelConstructorParams.js";
