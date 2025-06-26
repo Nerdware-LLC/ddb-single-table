0d21fe8f7d218032af2c4b6935841797d651a039
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2025-06-26 09:46:39 -0400

    refactor: rm unused 'ItemUpdateParameters'

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,7 +4,6 @@ export * from "./ItemCreationParameters.js";
 export * from "./ItemKeys.js";
 export * from "./ItemTypeFromSchema.js";
 export * from "./ItemTypeOpts.js";
-export * from "./ItemUpdateParameters.js";
 export * from "./NativeAttributeValue.js";
 export * from "./NestDepth.js";
 export * from "./SupportedAttributeValueType.js";
