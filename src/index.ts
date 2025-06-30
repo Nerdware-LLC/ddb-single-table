3857edb2dc6898dfd97aa05978f2599661c7e8b4
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2025-06-30 14:58:31 -0400

    refactor: streamline top-level exports

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
+export * from "./Expressions/index.js";
 export * from "./Model/index.js";
 export * from "./Schema/index.js";
 export * from "./Table/index.js";
 export * from "./types/index.js";
 
-// TYPE-ONLY EXPORTS:
-
-export type * from "./DdbClientWrapper/types/batchOperationTypes.js";
-
-export type { GenerateUpdateExpressionOpts } from "./Expressions/UpdateExpression/types.js";
-export type { WhereQueryComparisonObject } from "./Expressions/WhereQuery/types.js";
-
-export type * from "./Model/types/clientParamsForModel.js";
-export type * from "./Model/types/ModelConstructorParams.js";
+// ADDITIONAL TYPE-EXPORTS FROM INTERNAL MODULES:
+export type * from "./DdbClientWrapper/types/BatchRequestFunction.js";
